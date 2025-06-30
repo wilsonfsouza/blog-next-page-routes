d571e9e5234af7e3f1e361da648c81532c340877
Wilson Franca <wilson.franca.92@gmail.com>
2025-06-30 10:58:38 -0700

    feat: update header to use logo component

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,6 @@
 import Link from 'next/link'
 import { ActiveLink } from '../active-link'
+import { Logo } from '../logo'
 import { Button } from '../ui/button'
 
 export const Header = () => {
@@ -7,7 +8,7 @@ export const Header = () => {
     <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
-          <Link href="/">Logo</Link>
+          <Logo usesAltLogoOnMobile />
 
           <nav className="flex items-center gap-6">
             <ActiveLink href="/">Home</ActiveLink>
