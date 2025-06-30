d6f58a58b9ee75a24c6d297d48ffc2b34a0daa96
Wilson Franca <wilson.franca.92@gmail.com>
2025-06-30 12:44:55 -0700

    feat: add hero section

---
+++ src/components/hero-section/hero-section.tsx
@@ -0,0 +1,58 @@
+import { ArrowRight, Clock, Store } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+import { Button } from '../ui/button'
+
+export const HeroSection = () => {
+  return (
+    <section className="container relative mt-24 flex items-center justify-center md:mt-16 lg:mt-16">
+      <div className="grid min-h-[20rem] grid-cols-1 items-center gap-8 md:h-[36rem] md:grid-cols-2">
+        <div className="flex flex-col items-center justify-center gap-6 md:items-start md:gap-8 lg:items-start lg:gap-8">
+          <h1 className="text-heading-hg text-center text-gray-100 md:text-left lg:text-left">
+            Sell your products as an affiliate in only one place
+          </h1>
+
+          <div className="flex flex-col items-center justify-center gap-10 md:items-start md:gap-14 lg:items-start lg:gap-14">
+            <div className="flex flex-col gap-1 md:gap-2 lg:gap-2">
+              <div className="flex items-center gap-3">
+                <Clock className="h-4 w-4 text-cyan-100 md:h-5 md:w-5 lg:h-5 lg:w-5" />
+                <span className="text-body-sm text-gray-200">
+                  Create your site in less than 5 minutes
+                </span>
+              </div>
+
+              <div className="flex items-center gap-3">
+                <Store className="h-4 w-4 text-cyan-100 md:h-5 md:w-5 lg:h-5 lg:w-5" />
+                <span className="text-body-sm text-gray-200">
+                  Follow up and optimize your online store
+                </span>
+              </div>
+            </div>
+
+            <div className="flex flex-col items-center gap-4 text-white">
+              <Button className="w-fit rounded-full" asChild>
+                <Link href="/create-store">
+                  Create free store
+                  <ArrowRight />
+                </Link>
+              </Button>
+
+              <p className="text-body-xs text-gray-300">
+                No credit card needed
+              </p>
+            </div>
+          </div>
+        </div>
+        <div className="items center relative order-first hidden h-[20rem] justify-center md:order-last md:flex md:h-full lg:flex">
+          <Image
+            src="/hero-section.svg"
+            alt="Illustration with store, tag, and bag icons"
+            width={200}
+            height={400}
+            className="h-full w-auto object-contain"
+          />
+        </div>
+      </div>
+    </section>
+  )
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
-import { Geist, Geist_Mono } from 'next/font/google'
-
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-})
-
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
-  subsets: ['latin'],
-})
+import { HeroSection } from '@/components/hero-section'
 
 export default function Home() {
   return (
-    <div
-      className={`${geistSans.className} ${geistMono.className} grid min-h-screen grid-rows-[20px_1fr_20px] items-center justify-items-center gap-16 p-8 pb-20 font-[family-name:var(--font-geist-sans)] sm:p-20`}
-    ></div>
+    <>
+      <article className="flex flex-col">
+        <HeroSection />
+      </article>
+    </>
   )
 }
