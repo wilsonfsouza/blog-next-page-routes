baf9a26a0855f742721d3f9432b4bf77c7f0d636
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-03 11:01:02 -0700

    fix(cta-section): corrected store icon placement

--- src/templates/landing-page/sections/call-to-action/call-to-action.tsx
+++ src/templates/landing-page/sections/call-to-action/call-to-action.tsx
@@ -6,12 +6,11 @@ export const CallToAction = () => {
   return (
     <section className="relative bg-gradient-to-b from-cyan-950/20 to-gray-700 py-14 md:pb-[4.5rem] md:pt-[5.5rem]">
       <div className="absolute inset-0 bg-[url('/background-images/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
-
+      <span className="absolute left-1/2 top-0 flex h-14 w-14 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-cyan-300 md:h-16 md:w-16">
+        <Store className="h-7 w-7 text-cyan-100 md:h-8 md:w-8" />
+      </span>
       <div className="container relative">
         <div className="flex flex-col items-center gap-8 md:gap-10">
-          <span className="flex h-14 w-14 items-center justify-center rounded-full bg-cyan-300 md:h-16 md:w-16">
-            <Store className="h-7 w-7 text-cyan-100 md:h-8 md:w-8" />
-          </span>
           <h2 className="text-balance text-center font-sans text-heading-md text-gray-100 md:text-heading-xl">
             Create an online store and start selling today
           </h2>
