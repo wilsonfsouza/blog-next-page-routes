07c337e43c5bbcc11537476adf8242674507759f
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-03 10:27:03 -0700

    feat(cta-section): add bg image

--- src/components/call-to-action/call-to-action.tsx
+++ src/components/call-to-action/call-to-action.tsx
@@ -4,13 +4,15 @@ import { Button } from '../ui/button'
 
 export const CallToAction = () => {
   return (
-    <section className="bg-gradient-to-b from-cyan-950/20 to-gray-700 py-14 md:pb-[4.5rem] md:pt-[5.5rem]">
-      <div className="container">
+    <section className="relative bg-gradient-to-b from-cyan-950/20 to-gray-700 py-14 md:pb-[4.5rem] md:pt-[5.5rem]">
+      <div className="absolute inset-0 bg-[url('/background-images/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+
+      <div className="container relative">
         <div className="flex flex-col items-center gap-8 md:gap-10">
           <span className="flex h-14 w-14 items-center justify-center rounded-full bg-cyan-300 md:h-16 md:w-16">
             <Store className="h-7 w-7 text-cyan-100 md:h-8 md:w-8" />
           </span>
-          <h2 className="text-balance font-sans text-heading-md text-gray-100 md:text-heading-xl">
+          <h2 className="text-balance text-center font-sans text-heading-md text-gray-100 md:text-heading-xl">
             Create an online store and start selling today
           </h2>
 
