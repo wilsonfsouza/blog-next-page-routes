3dc185c076a94520c2f706964b41f4dbea74a461
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-03 10:55:56 -0700

    fix(footer/header): update imports to be absolute

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -1,11 +1,11 @@
+import { ActiveLink } from '@/components/active-link'
+import { Logo } from '@/components/logo'
+import { Button } from '@/components/ui/button'
 import Link from 'next/link'
-import { ActiveLink } from '../active-link'
-import { Logo } from '../logo'
-import { Button } from '../ui/button'
 
 export const Header = () => {
   return (
-    <header className="fixed top-0 z-50 w-full border-b border-white/10 bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
+    <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur">
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <div className="flex h-16 items-center justify-between">
           <Logo usesAltLogoOnMobile />
