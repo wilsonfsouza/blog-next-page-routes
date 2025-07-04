1eb59cba12baf3b50534a7507059afd27dd3ac03
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-04 11:03:35 -0700

    fix(header): correct lateral padding and margins, added container class to match design

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -6,7 +6,7 @@ import Link from 'next/link'
 export const Header = () => {
   return (
     <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur">
-      <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+      <div className="container">
         <div className="flex h-16 items-center justify-between">
           <Logo usesAltLogoOnMobile />
 
