820525f78f9b59e10de0c0418936d0e83f6c095a
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-06 12:02:03 -0700

    fix(post-grid-card): change parent div to section

--- src/templates/blog/components/post-grid-card/post-grid-card.tsx
+++ src/templates/blog/components/post-grid-card/post-grid-card.tsx
@@ -4,8 +4,8 @@ interface BlogPostGridCardProps {
 
 export const BlogPostGridCard = ({ children }: BlogPostGridCardProps) => {
   return (
-    <div className="container grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
+    <section className="container grid grid-cols-1 gap-4 md:grid-cols-2 md:gap-6 lg:grid-cols-3">
       {children}
-    </div>
+    </section>
   )
 }
