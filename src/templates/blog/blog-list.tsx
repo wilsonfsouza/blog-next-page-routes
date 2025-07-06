835481b4fcfd41038de84c155f7f7c0ad2724b4d
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-06 11:53:44 -0700

    feat: integrated blog search with query

--- src/templates/blog/blog-list.tsx
+++ src/templates/blog/blog-list.tsx
@@ -1,5 +1,6 @@
 import { Search } from '@/components/search'
 import { allPosts } from 'contentlayer/generated'
+import { Inbox } from 'lucide-react'
 import { useRouter } from 'next/router'
 import { BlogPostCard } from './components/post-card'
 import { BlogPostGridCard } from './components/post-grid-card'
@@ -11,7 +12,13 @@ export function BlogList() {
   const DEFAULT_PAGE_TITLE = 'Tips and strategies to boost your business'
   const pageTitle = query ? `Search results for "${query}"` : DEFAULT_PAGE_TITLE
 
-  const posts = allPosts
+  const posts = query
+    ? allPosts.filter((post) =>
+        post.title.toLowerCase()?.includes(query.toLowerCase())
+      )
+    : allPosts
+
+  const hasPosts = posts.length > 0
   return (
     <div className="flex flex-col gap-6 px-4 py-5 md:gap-14 md:px-0 md:py-20">
       <header>
@@ -29,22 +36,36 @@ export function BlogList() {
         </div>
       </header>
 
-      <BlogPostGridCard>
-        {posts.map((post) => (
-          <BlogPostCard
-            key={post._id}
-            title={post.title}
-            description={post.description}
-            date={new Date(post.date).toLocaleDateString('en-US')}
-            slug={post.slug}
-            image={post.image}
-            author={{
-              avatar: post.author.avatar,
-              name: post.author.name,
-            }}
-          />
-        ))}
-      </BlogPostGridCard>
+      {hasPosts && (
+        <BlogPostGridCard>
+          {posts.map((post) => (
+            <BlogPostCard
+              key={post._id}
+              title={post.title}
+              description={post.description}
+              date={new Date(post.date).toLocaleDateString('en-US')}
+              slug={post.slug}
+              image={post.image}
+              author={{
+                avatar: post.author.avatar,
+                name: post.author.name,
+              }}
+            />
+          ))}
+        </BlogPostGridCard>
+      )}
+
+      {!hasPosts && (
+        <div className="container">
+          <div className="flex flex-col items-center justify-center gap-4 rounded-lg border-2 border-dashed border-gray-300 p-8 md:p-12">
+            <Inbox className="h-12 w-12 text-cyan-100" />
+
+            <p className="text-center text-body-md text-gray-100">
+              No posts were found.
+            </p>
+          </div>
+        </div>
+      )}
     </div>
   )
 }
