38a31fda80e7cc4b3fe7e04fb33e188596b62df5
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-30 12:10:19 -0700

    refactor(blog-post-page): move blog post page to app router format

---
+++ src/app/blog/[slug]/page.tsx
@@ -0,0 +1,38 @@
+import { BlogPost } from '@/templates/blog'
+import { allPosts } from 'contentlayer/generated'
+import { notFound } from 'next/navigation'
+
+interface BlogPostPageProps {
+  params: Promise<{
+    slug: string
+  }>
+}
+
+export const revalidate = 60 * 60 * 24 // 1 day
+export const dynamicParams = true
+
+export async function generateStaticParams() {
+  const sortedPosts = allPosts.sort(
+    (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
+  )
+  const recentPosts = sortedPosts.slice(0, 5)
+  const paths = recentPosts.map((post) => ({
+    slug: post.slug,
+  }))
+
+  return paths
+}
+
+export default async function BlogPostPage({ params }: BlogPostPageProps) {
+  const { slug } = await params
+
+  const post = allPosts.find(
+    (post) => post?.slug.toLowerCase() === slug.toLowerCase()
+  )
+
+  if (!post) {
+    notFound()
+  }
+
+  return <BlogPost post={post} />
+}
