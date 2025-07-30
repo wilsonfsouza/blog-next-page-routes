949b8788ec9889ffe6824f0fdcfded533d8cb7a7
Wilson Franca <wilson.franca.92@gmail.com>
2025-07-30 12:12:53 -0700

    chore: remove pages routes that were migrated to app router

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body>
+      <body className="antialiased">
         <Layout>{children}</Layout>
       </body>
     </html>
