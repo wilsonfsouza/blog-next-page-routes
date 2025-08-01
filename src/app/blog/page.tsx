fc7ffcd5825a6812ca191930dadbee578841aaa7
Wilson Franca <wilson.franca.92@gmail.com>
2025-08-01 11:39:09 -0700

    feat: add static and dynamic metadata with og-image to pages

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,5 +1,28 @@
 import { BlogList } from '@/templates/blog'
 import { allPosts } from 'contentlayer/generated'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Blog',
+  description: 'STips and strategies to boost your business',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Blog',
+    description: 'STips and strategies to boost your business',
+    url: 'https://blog-next-page-routes.vercel.app/og-image.jpg',
+    siteName: 'Site.',
+    locale: 'en-US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://blog-next-page-routes.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.',
+      },
+    ],
+  },
+}
 
 export default function BlogPage() {
   const sortedPosts = allPosts.sort(
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,27 @@
 import { LandingPage } from '@/templates/landing-page'
+import type { Metadata } from 'next'
+
+export const metadata: Metadata = {
+  title: 'Site.',
+  description: 'Sell your products as an affiliate in only one place',
+  robots: 'index, follow',
+  openGraph: {
+    title: 'Site.',
+    description: 'Sell your products as an affiliate in only one place',
+    url: 'https://blog-next-page-routes.vercel.app/og-image.jpg',
+    siteName: 'Site.',
+    locale: 'en-US',
+    type: 'website',
+    images: [
+      {
+        url: 'https://blog-next-page-routes.vercel.app/og-image.jpg',
+        width: 800,
+        height: 600,
+        alt: 'Site.',
+      },
+    ],
+  },
+}
 
 export default function Homepage() {
   return <LandingPage />
