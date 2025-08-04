b60b376497f6f11f6fca5486db16b5ce98c3df55
Wilson Franca <wilson.franca.92@gmail.com>
2025-08-04 14:51:58 -0700

    fix(call-to-action): correct opacity of bg image

--- src/components/layout/call-to-action/call-to-action.tsx
+++ src/components/layout/call-to-action/call-to-action.tsx
@@ -5,7 +5,9 @@ import Link from 'next/link'
 export const CallToAction = () => {
   return (
     <section className="relative bg-gradient-to-b from-cyan-950/20 to-gray-700 py-14 md:pb-[4.5rem] md:pt-[5.5rem]">
-      <div className="absolute inset-0 bg-[url('/background-images/background-footer.svg')] bg-cover bg-center bg-no-repeat opacity-90" />
+      <div className="opacity-90">
+        <div className="absolute inset-0 bg-[url('/background-images/background-footer.svg')] bg-cover bg-center bg-no-repeat" />
+      </div>
       <span className="absolute left-1/2 top-0 flex h-14 w-14 -translate-x-1/2 -translate-y-1/2 items-center justify-center rounded-full bg-cyan-300 md:h-16 md:w-16">
         <Store className="h-7 w-7 text-cyan-100 md:h-8 md:w-8" />
       </span>
