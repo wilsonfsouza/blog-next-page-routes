7bb11440e79b0ad404b6186d708768b42d541c34
Wilson Franca <wilson.franca.92@gmail.com>
2025-08-07 13:09:30 -0700

    fix(header-mobile): prevent layout shift when navigation on mobile opens and scrollbar is hidden

--- src/components/layout/header/header.tsx
+++ src/components/layout/header/header.tsx
@@ -25,7 +25,7 @@ const menuItems: MenuItem[] = [
 
 export const Header = () => {
   return (
-    <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 fixed top-0 z-50 w-full border-b border-white/10 backdrop-blur">
+    <header className="bg-background/95 supports-[backdrop-filter]:bg-background/60 fixed top-0 z-50 w-[100vw] border-b border-white/10 backdrop-blur">
       <div className="container">
         <div className="flex h-16 items-center justify-between">
           <Logo usesAltLogoOnMobile fetchPriority="high" />
